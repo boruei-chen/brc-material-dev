f90c031f72ede066259fe2cb2f05ca87f720eba1
BO-RUEI CHEN <boruei.brc@gmail.com>
2023-02-16 18:08:21 +0800

    refactor: 調整 Button Component (Test) stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Button from './Button';
 
-const Meta: ComponentMeta<typeof Button> = { title: 'components/Button/Button', component: Button };
+const Meta: ComponentMeta<typeof Button> = { title: 'components/Button', component: Button };
 const Story: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Story.bind({});
